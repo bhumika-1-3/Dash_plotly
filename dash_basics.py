f9775f2692a7536da854861e97c8b8b33d7ff798
Bhumika Mange <80103105+bhumika-1-3@users.noreply.github.com>
2022-04-02 20:05:18 +0530

    bar chart added

--- dash_basics.py
+++ dash_basics.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as go
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
 
 # Read the airline data into pandas dataframe
 airline_data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
@@ -32,26 +33,49 @@ fig3 = go.Figure(data=go.Scatter(
     x=line_data['Month'], y=line_data['ArrDelay']))
 fig3.update_layout(title='average delay ',
                    xaxis_title='months', yaxis_title='delay ')
-                   
+
 # Create a dash application
 app = dash.Dash(__name__)
+# add callback decorator
+@app.callback(Output(component_id = 'bar-plot',component_property = 'figure'),
+               Input(component_id = 'input-year',component_property = 'value'))
+# Add computation to callback function and return graph
+def get_graph(entered_year):
+    # Select data based on the entered year
+    df =  airline_data[airline_data['Year']==int(entered_year)]
+    
+    g1 = df.groupby(['Reporting_Airline'])['Flights'].sum().nlargest(10).reset_index()
+
+
+    # 
+    
+    fig4 = px.bar(g1 , x='Reporting_Airline' , y='Flights' , title = 'top 10 airlines')
+    fig4.update_layout()
+    return fig4
+
 
 # Get the layout of the application and adjust it.
 # Create an outer division using html.Div and add title to the dashboard using html.H1 component
 # Add description about the graph using HTML P (paragraph) component
 # Finally, add graph component.
 app.layout = html.Div(children=[html.H1('Airline Dashboard', style={'textAlign': 'center', 'color': '#503D36', 'font-size': 70}),
+                                html.P('Bar chart', style={
+                                    'textAlign': 'center', 'color': '#F57241'}),
+                                html.Div(["Input Year", dcc.Input(id = 'input-year' , value = 2010 , type = 'number'), ]),
+                                dcc.Graph(id = 'bar-plot'),
+
+
                                 html.P('Pie chart', style={
                                     'textAlign': 'center', 'color': '#F57241'}),
                                 # display the pie created by plotly
                                 dcc.Graph(figure=fig),
 
                                 html.P("Scattered plot", style={
-                                       'textAlign': 'center', 'color': '#F57241'}),
+                                    'textAlign': 'center', 'color': '#F57241'}),
                                 dcc.Graph(figure=fig2),
 
                                 html.P("Line chart", style={
-                                       'textAlign': 'center', 'color': '#F57241'}),
+                                    'textAlign': 'center', 'color': '#F57241'}),
                                 dcc.Graph(figure=fig3)
                                 ])
 
